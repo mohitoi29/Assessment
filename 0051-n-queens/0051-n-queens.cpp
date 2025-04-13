b2a86b278c4a0d0b0d57ab388a2c8b2b34ac2849
mohitoi29 <147608567+mohitoi29@users.noreply.github.com>
2025-04-13 11:01:01 +0530

    Time: 3 ms (65.15%), Space: 10.3 MB (56.57%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,38 +1,44 @@
 class Solution {
     private:
-    bool fill(int col,int row,vector<string>& board,int n){
-        int duprow=row;
-        int dupcol=col;
-        while(row>=0 && col>=0){
-            if(board[row][col]=='Q') return false;
-            row--;
-            col--;
-        }
-        row=duprow;
-        col=dupcol;
-        while(col>=0){
-            if(board[row][col]=='Q') return false;
-            col--;
-        }
-        row=duprow;
-        col=dupcol;
-        while(col>=0 && row<n){
-            if(board[row][col]=='Q') return false;
-            col--;
-            row++;
-        }
-        return true;
-    }
-        void func(int col,vector<vector<string>>& sol,vector<string>& board,int n){
+    // bool fill(int col,int row,vector<string>& board,int n){
+    //     int duprow=row;
+    //     int dupcol=col;
+    //     while(row>=0 && col>=0){
+    //         if(board[row][col]=='Q') return false;
+    //         row--;
+    //         col--;
+    //     }
+    //     row=duprow;
+    //     col=dupcol;
+    //     while(col>=0){
+    //         if(board[row][col]=='Q') return false;
+    //         col--;
+    //     }
+    //     row=duprow;
+    //     col=dupcol;
+    //     while(col>=0 && row<n){
+    //         if(board[row][col]=='Q') return false;
+    //         col--;
+    //         row++;
+    //     }
+    //     return true;
+    // }
+        void func(int col,vector<vector<string>>& sol,vector<string>& board,int n,vector<int>& left,vector<int>& leftUpDiagonal,vector<int>& leftDownDiagonal){
             if(col==n){
                 sol.push_back(board);
                 return;
             }
             for(int row=0;row<n;row++){
-                if(fill(col,row,board,n)){
+                if(left[row]==0 && leftUpDiagonal[(n-1)+(col-row)]==0 && leftDownDiagonal[row+col]==0){
+                    left[row]=1;
+                    leftUpDiagonal[(n-1)+(col-row)]=1 ;
+                      leftDownDiagonal[row+col]=1;
                     board[row][col]='Q';
-                    func(col+1,sol,board,n);
+                    func(col+1,sol,board,n,left,leftUpDiagonal,leftDownDiagonal);
                     board[row][col]='.';
+                    left[row]=0;
+                     leftUpDiagonal[(n-1)+(col-row)]=0 ;
+                      leftDownDiagonal[row+col]=0;
                 }
             }
         }
@@ -44,7 +50,10 @@ public:
         for(int i=0;i<n;i++){
             board[i]=s;
         }
-        func(0,sol,board,n);
+        vector<int> left(n,0);
+        vector<int> leftUpDiagonal(2*n-1,0);
+        vector<int> leftDownDiagonal(2*n-1,0);
+        func(0,sol,board,n,left,leftUpDiagonal,leftDownDiagonal);
         return sol;
     }
 };
