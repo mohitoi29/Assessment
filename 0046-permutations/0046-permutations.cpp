1aa984af351416deb5983251df9db67a2d491b5e
mohitoi29 <147608567+mohitoi29@users.noreply.github.com>
2025-04-13 17:16:00 +0530

    Time: 0 ms (100%), Space: 9.2 MB (100%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,26 +1,21 @@
 class Solution {
 private:
- void func(  vector<vector<int>>& sol,vector<int>& subAns,vector<int>& nums, vector<bool>& check){
-    if(subAns.size()==nums.size()){
-        sol.push_back(subAns);
-        return ;
+    void perm(vector<int>& nums, vector<vector<int>> &ans,int index){
+        if(index>=nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=index;i<nums.size();i++){
+            swap(nums[i],nums[index]);
+            perm(nums,ans ,index+1);
+            swap(nums[i],nums[index]);
+        }
     }
-    for(int i=0;i<nums.size();i++){
-    if(!check[i]){
-        subAns.push_back(nums[i]);
-        check[i]=true;
-        func(sol,subAns,nums,check);
-        check[i]=false;
-        subAns.pop_back();
-    }
-    }
- }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> sol;
-        vector<int> subAns;
-        vector<bool> check(nums.size(),false);
-        func(sol,subAns,nums,check);
-        return sol;
+        vector<vector<int>> ans;
+        int index=0;
+        perm(nums,ans,index);
+        return ans;
     }
 };
